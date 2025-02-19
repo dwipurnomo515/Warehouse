ad08f5fec9290c97bb41c30f0544dba77719985d
Dwi Purnomo <dwiask5151@.com>
2025-02-19 14:49:42 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,23 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\StockTransactionController;
 use App\Http\Controllers\SupplierController;
+use App\Http\Middleware\AdminMiddleware;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return Auth::check() ? redirect()->route('dashboard') : redirect()->route('login');
 });
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
-Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard')->middleware('auth');
-Route::resource('products', ProductController::class);
-Route::resource('stock_transactions', StockTransactionController::class)->only(['index', 'create', 'store']);
-Route::resource('suppliers', SupplierController::class);
-Route::resource('purchases', PurchaseController::class);
-Route::resource('customers', CustomerController::class);
+
+
+Route::middleware(['auth', AdminMiddleware::class])->group(function () {
+    Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard')->middleware('auth');
+    Route::resource('products', ProductController::class);
+    Route::resource('stock_transactions', StockTransactionController::class)->only(['index', 'create', 'store']);
+    Route::resource('suppliers', SupplierController::class);
+    Route::resource('purchases', PurchaseController::class);
+    Route::resource('customers', CustomerController::class);
+});
